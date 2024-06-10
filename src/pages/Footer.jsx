2ece71a3c1435c5a168698d7f7c1482a49537eb5
KenishDahal <kennisdahal@gmail.com>
2024-06-10 12:38:23 +0545

    Footer commit

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
+import logo from "../images/logo-remit.png";
+
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <div className='h6'>Revamp</div>
-      <div className='eyebrow'>All rights reserved  ©2024 Revamp</div>
+    <div className="footer">
+      {/* <div className="h6">Revamp</div> */}
+      <figure className="">
+        <img src={logo} alt="logo" />
+      </figure>
+      <div className="eyebrow">All rights reserved ©2024 Revamp</div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Footer
+export default Footer;
